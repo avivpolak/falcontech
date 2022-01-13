10ba670181213f9d29261baa149a2804643f9439
Gavriel Frant <gavril44@gmail.com>
2022-01-13 11:07:07 +0200

    updated navbar css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import './App.css';
-import MainSection from "./components/MainSection"
+import "./App.css";
+import MainSection from "./components/MainSection";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
-
-<MainSection/>
+      <Navbar />
+      <MainSection />
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,22 @@
-import React from 'react'
+import React from "react";
 
 export default function Navbar() {
-    return (
-        <div>
-                   <div className="navbar">
-                    <a href="#home" className="navbarItem">
-                        home
-                    </a>
-                    <a href="#about" className="navbarItem">
-                        about
-                    </a>
-                    <a href="#technology" className="navbarItem">
-                        technology
-                    </a>
-                    <a href="#contact" className="navbarItem">
-                        contact
-                    </a>
-                </div>
-        </div>
-    )
+  return (
+    <div className="navbar">
+      <div className="navbar-items-container">
+        <a href="#home" className="navbarItem">
+          home
+        </a>
+        <a href="#about" className="navbarItem">
+          about
+        </a>
+        <a href="#technology" className="navbarItem">
+          technology
+        </a>
+        <a href="#contact" className="navbarItem">
+          contact
+        </a>
+      </div>
+    </div>
+  );
 }
